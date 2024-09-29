c76bb5364d0ec15d9c90d5bf0b4c9af32acf038a
Aly-exe <alibnraslan@gmail.com>
2024-09-29 13:52:22 +0200

    clear unnecessary imports and refactoring code

--- lib/features/auth/presentation/view/google_login_auth_screen.dart
+++ lib/features/auth/presentation/view/google_login_auth_screen.dart
@@ -1,10 +1,5 @@
-import 'dart:developer';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:linkyou_task/features/auth/presentation/view/widgets/signin_with_google_widget.dart';
-import 'package:linkyou_task/features/auth/presentation/view_model/auth_view_model.dart';
-import 'package:linkyou_task/features/auth/presentation/view_model/post_new_user_model.dart';
 
 class GoogleAuth extends StatelessWidget {
   const GoogleAuth({super.key});
@@ -13,7 +8,6 @@ class GoogleAuth extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      // backgroundColor: Colors.grey[300],
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
--- lib/features/home/presentation/view/users_screen.dart
+++ lib/features/home/presentation/view/users_screen.dart
@@ -1,24 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:linkyou_task/features/home/presentation/view/widgets/custom_circural_indecator.dart';
 import 'package:linkyou_task/features/home/presentation/view/widgets/user_card_widget.dart';
 import 'package:linkyou_task/features/home/presentation/view_model/get_user_data_bloc/cubit/get_user_data_cubit.dart';
 import 'package:linkyou_task/features/home/presentation/view_model/get_user_data_bloc/cubit/get_user_data_state.dart';
-import 'package:linkyou_task/features/home/presentation/view_model/get_users_view_model.dart';
 
-class UserScreen extends StatefulWidget {
+class UserScreen extends StatelessWidget {
   const UserScreen({super.key});
-
-  @override
-  State<UserScreen> createState() => _UserScreenState();
-}
-
-class _UserScreenState extends State<UserScreen> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    GetUsersViewModel.getUserData();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +15,24 @@ class _UserScreenState extends State<UserScreen> {
       create: (context) => GetUserDataBloc()..getUserData(),
       child: BlocBuilder<GetUserDataBloc, GetUserDataStates>(
         builder: (context, state) {
-          return  Scaffold(
-            body: state is LoadingUserDataState? Center(child: CircularProgressIndicator()) : state is SuccessGetUserDataState ? Padding(
-                padding: EdgeInsets.all(10),
-                child: ListView.builder(
-                    itemCount: state.userData.userData!.length,
-                    itemBuilder: (contxet, index) {
-                      return UserCardWidget(state.userData,index);
-                    })) : const Center(child: Text("We Can't Fetch Data"),)
-          );
+          return Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                title: Text('Users'),
+              ),
+              body: state is LoadingUserDataState
+                  ? const CustomCircuralIndictaor()
+                  : state is SuccessGetUserDataState
+                      ? Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: ListView.builder(
+                              itemCount: state.userData.userData!.length,
+                              itemBuilder: (contxet, index) {
+                                return UserCardWidget(state.userData, index);
+                              }))
+                      : const Center(
+                          child: Text("We Can't Fetch Data"),
+                        ));
         },
       ),
     ));
